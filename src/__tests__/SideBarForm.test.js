7a9df53dd18f8c2bede243e529200c614d9b1f6e
Aidan Mueller <Aidan_Mueller@hotmail.co.uk>
2020-07-26 21:00:46 +0100

    sidebar tests done

--- src/__tests__/SideBarForm.test.js
+++ src/__tests__/SideBarForm.test.js
@@ -16,10 +16,39 @@ describe("SideBarForm", () => {
         <MemoryRouter><SideBarForm {...props}/></MemoryRouter> )
         expect(asFragment).toMatchSnapshot();
     });
-    xit("renders a form", () => {
+    it("renders a form", () => {
         const { getByTestId } = render (
         <MemoryRouter><SideBarForm {...props}/></MemoryRouter> )
         const form = getByTestId("sideBarForm");
         expect(form).toBeInTheDocument();
+    });
+    it("renders an input for the form", () => {
+        const { getByTestId } = render (
+        <MemoryRouter><SideBarForm {...props}/></MemoryRouter> )
+        const input = getByTestId("input-id");
+        expect(input).toBeInTheDocument();
+    });
+    it("renders a search button", () => {
+        const { getByRole } = render (
+        <MemoryRouter><SideBarForm {...props}/></MemoryRouter> )
+        const btn = getByRole("button");
+        expect(btn).toBeInTheDocument();
+        expect(btn).toHaveTextContent("Search");
+    });
+    it("changes the state when input is changed", () => {
+        const { getByTestId } = render (
+        <MemoryRouter><SideBarForm {...props}/></MemoryRouter> )
+        const handleChange = props.handleChange;
+        const input = getByTestId("input-id");
+        fireEvent.change(input, { target: { value: "randomQuery"}});
+        expect(handleChange).toHaveBeenCalled();
+    });
+    it("calls handleSearch on submission of form", () => {
+        const { getByTestId } = render (
+        <MemoryRouter><SideBarForm {...props}/></MemoryRouter> )
+        const handleSearch = props.handleSearch;
+        const form = getByTestId("sideBarForm"); 
+        fireEvent.submit(form);
+        expect(handleSearch).toHaveBeenCalled()
     });
 });
